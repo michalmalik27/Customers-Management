c7f94ef502190ef2edf197e318fd2329bbef9dd4
michalmalik27 <82401508+michalmalik27@users.noreply.github.com>
2021-05-19 03:46:51 +0300

    Update Customer.cs
    Remove todo commrnts

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -11,18 +11,15 @@ namespace ServerSide.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int CustomerId { get; set; }
 
-        //TODO
         [RegularExpression(@"^\d{9}$", ErrorMessage = "Id Number not valid")]
         [Required(ErrorMessage = "ID Number is required")]
         public int IdNumber { get; set; }
 
-        //TODO
         [RegularExpression(@"^[א-ת''-'\s]{1,20}$", ErrorMessage = "Hebrew name contains not allowed characters")]
         [Required(ErrorMessage = "Hebrew name is required")]
         [StringLength(20, ErrorMessage = "Hebrew name can't be longer than 20 characters")]
         public string CustomerHebName { get; set; }
 
-        //TODO
         [RegularExpression(@"^[a-zA-Z''-'\s]{1,15}$", ErrorMessage = "English name contains not allowed characters")]
         [Required(ErrorMessage = "English name is required")]
         [StringLength(15, ErrorMessage = "English name can't be longer than 15 characters")]
@@ -43,7 +40,6 @@ namespace ServerSide.Models
         [Required(ErrorMessage = "Bank branch is required")]
         public int BankBranch { get; set; }
 
-        //TODO
         [RegularExpression(@"^\d{1,10}$")]
         [Required(ErrorMessage = "Accounting number is required")]
         public int AccountingNumber { get; set; }
